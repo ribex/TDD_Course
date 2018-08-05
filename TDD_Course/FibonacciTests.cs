bc95f7eeebc7a0bdc1f4bc9077a20893054c4afd
Rebecca Wagaman <rebecca.wagaman@gmail.com>
2018-08-05 19:15:17 -0400

    Fibonacci tests - initial

---
+++ TDD_Course/FibonacciTests.cs
@@ -0,0 +1,33 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TDD_Course
+{
+    [TestFixture]
+    public class FibonacciTests
+    {
+        [Test]
+        public void TestFibonacci()
+        {
+            Assert.AreEqual(0, GetFibonacci(0));
+            Assert.AreEqual(1, GetFibonacci(1));
+
+        }
+
+        private int GetFibonacci(int index)
+        {
+            if (index == 1)
+            {
+                return 1;
+            }
+            else
+            {
+                return 0;
+            }
+        }
+    }
+}
