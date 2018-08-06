3427e1a226128d8f0b1c656e22ddfcf682e8f563
Rebecca Wagaman <rebecca.wagaman@gmail.com>
2018-08-06 19:31:42 -0400

    Roman tests

--- TDD_Course/RomanNumeralsTest.cs
+++ TDD_Course/RomanNumeralsTest.cs
@@ -11,15 +11,95 @@ namespace TDD_Course
     [TestFixture()]
     public class RomanNumeralsTest
     {
-        [Test]
-        public void Parse()
+        [TestCase("I", 1)]
+        [TestCase("V", 5)]
+        [TestCase("X", 10)]
+        [TestCase("L", 50)]
+        [TestCase("C", 100)]
+        [TestCase("D", 500)]
+        [TestCase("M", 1000)]
+        [TestCase("II", 2)]
+        [TestCase("III", 3)]
+        [TestCase("IV", 4)]
+        [TestCase("VI", 6)]
+        [TestCase("IX", 9)]
+        [TestCase("XL", 40)]
+
+        public void Parse(string roman, int arabic)
         {
-            Assert.AreEqual(ParseRoman("I"), 1);
+            Assert.AreEqual(ParseRoman(roman), arabic);
         }
 
-        private int ParseRoman(string s)
+        private int ParseRoman(string roman)
         {
-            return -1000;
+            int number = 0;
+
+            if (roman == "M")
+            {
+                number = 1000;
+            }
+
+            if (roman == "D")
+            {
+                number = 500;
+            }
+
+            if (roman == "C")
+            {
+                number = 100;
+            }
+
+            if (roman == "L")
+            {
+                number = 50;
+            }
+
+            if (roman == "XL")
+            {
+                number = 40;
+            }
+
+            if (roman == "X")
+            {
+                number = 10;
+            }
+
+            if (roman == "IX")
+            {
+                number = 9;
+            }
+
+            if (roman == "VI")
+            {
+                number = 6;
+            }
+
+            if (roman == "V")
+            {
+                number = 5;
+            }
+
+            if (roman == "IV")
+            {
+                number = 4;
+            }
+
+            if (roman == "III")
+            {
+                number = 3;
+            }
+
+            if (roman == "II")
+            {
+                number = 2;
+            }
+
+            if (roman == "I")
+            {
+                number = 1;
+            }
+
+            return number;
         }
     }
 }
