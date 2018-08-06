70d4cd282be2c3c75536cf3dccf767f06b43bb94
Rebecca Wagaman <rebecca.wagaman@gmail.com>
2018-08-06 18:55:07 -0400

    Begin RomanNumeralsTest

---
+++ TDD_Course/RomanNumeralsTest.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using NUnit.Framework.Internal;
+
+namespace TDD_Course
+{
+    [TestFixture()]
+    public class RomanNumeralsTest
+    {
+        [Test]
+        public void Parse()
+        {
+            Assert.AreEqual(ParseRoman("I"), 1);
+        }
+
+        private int ParseRoman(string s)
+        {
+            return -1000;
+        }
+    }
+}
